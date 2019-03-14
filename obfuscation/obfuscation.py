48592e700955094d00bb0c3372a7b1109dc5e81b
hmurovich.na@phystech.edu <nkhmurovich@parallels.com>
2019-03-14 17:09:06 +0300

    some alg ahaha

--- obfuscation/obfuscation.py
+++ obfuscation/obfuscation.py
@@ -1,23 +1,56 @@
-with open("input_file.txt", "r") as f:
-    n = int(f.readline())
+import functools
 
-a = []
-for i in range(n + 1):
-    a.append(i)
+def do(func):
+    @functools.wraps(func)
+    def OMG(*args, **kwargs):
+        return func(*args, **kwargs)
+    return OMG
+
+class IDontKnowWhatIsThis():
+    def __init__(self):
+        self.a = []
+        self.n = 0
+        self.doSomething(self)
+
+    @do
+    def doSomething(self, *args, **kwargs):
+        with open("input_file.txt", "r") as f:
+            self.n = int(f.readline())
+        self.doSomethingElse(self)
+
+    @do
+    def doSomethingElse(self, *args, **kwargs):
+        a = []
+        for i in range(self.n + 1):
+            a.append(i)
+        self.a = a[:]
+        self.doSomethingAgain(self)
+
+    @do
+    def doSomethingAgain(self, *args, **kwargs):
+        a = self.a
+        a.insert(1, 0)
+        self.a = a[:]
+        self.doSomethingSpecial()
+
+    @do
+    def doSomethingSpecial(self, *args, **kwargs):
+        a = self.a
+        i = 2
+        while i <= self.n:
+            if a[i] != 0:
+                j = i + i
+                while j <= self.n:
+                    a[j] = 0
+                    j = j + i
+            i += 1
+        self.doSomethingMan()
+
+    @do
+    def doSomethingMan(self, *args, **kwargs):
+        a = set(self.a)
+        a.remove(0)
+        print(a)
 
 
-a[1] = 0
-
-i = 2
-while i <= n:
-    if a[i] != 0:
-        j = i + i
-        while j <= n:
-            a[j] = 0
-            j = j + i
-    i += 1
-
-a = set(a)
-a.remove(0)
-a = sorted(a)
-print(a)
+IDontKnowWhatIsThis()
